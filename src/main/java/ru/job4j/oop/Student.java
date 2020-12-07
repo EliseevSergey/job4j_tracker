d83f44ff90bb003f767bfdf7846be243eb2f5b8e
sergeyeliseev <s.eliseev1@gmail.com>
2020-12-07 20:13:26 +0300

    update

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -2,20 +2,15 @@ package ru.job4j.oop;
 
 public class Student {
 
-    public void music() {
-        System.out.println("Tra tra ta");
-    }
-    public void song() {
-        System.out.println("I belive I Can fly");
+    public void music(String lyrics) {
+        System.out.println("I can sign a song : " + lyrics);
     }
 
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
-        petya.song();
-        petya.song();
-        petya.song();
+        String song = "I believe, I can fly";
+        String song2 = "We will rock you";
+        petya.music(song);
+        petya.music(song2);
     }
 }
