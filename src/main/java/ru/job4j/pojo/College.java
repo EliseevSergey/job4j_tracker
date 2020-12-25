3a90d8f9091aa5d143f55b9575e91e894511bf05
sergeyeliseev <s.eliseev1@gmail.com>
2020-12-25 21:13:06 +0300

    Нудятина

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -5,7 +5,6 @@ public class College {
         Student shkolota = new Student();
         shkolota.setSurname("Ivanov");
         shkolota.setGroup("1019/2");
-        shkolota.setCreated(); // мне не удалось разобраться, как внести дату в форме обьекта поля даты. надеюсь про это будет дальше
         System.out.println("Name: " + shkolota.getSurname() + " group: " + shkolota.getGroup() + " entrance: " + shkolota.getCreated());
     }
 }
