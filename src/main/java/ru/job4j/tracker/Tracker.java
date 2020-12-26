cd66bcc3bca20f169d549cdbb568c63f72d51c3f
sergeyeliseev <s.eliseev1@gmail.com>
2020-12-27 00:04:41 +0300

    5. Tracker - хранилище [#396656]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -14,12 +16,37 @@ public class Tracker {
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = items[index];
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] noNull = new Item[size];
+        int qty = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getName() != null) {
+                noNull[qty] = items[index];
+                qty++;
+            }
+            noNull = Arrays.copyOf(noNull, qty);
+        }
+        return noNull;
+    }
+
+    public Item[] findByName(String name) {
+        Item[] nameList = new Item[size];
+        int qty = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getName() == name) {
+                nameList[qty] = items[index];
+                qty++;
+            }
+            nameList = Arrays.copyOf(nameList, qty);
+        }
+        return nameList;
     }
 }
