7e1b0b027c62048052cd3453a975bb8990facc61
sergeyeliseev <s.eliseev1@gmail.com>
2020-12-27 23:37:34 +0300

    9. toString [#396655]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item pilot = new Item();
+        pilot.setName("Gagarin");
+        pilot.setId(1);
+        System.out.println(pilot);
+    }
+}
