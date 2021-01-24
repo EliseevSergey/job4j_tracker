81e6cc43b3f42b7cef64b73d2a0a4fd6bc185835
EliseevSergey <s.eliseev1@gmail.com>
2021-01-25 00:24:16 +0400

    5.1. Тестирование. Подготовка данных. [#396585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,7 +1,10 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
@@ -12,7 +15,31 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         StartUI.createItem(input, tracker);
         Item created = tracker.findAll()[0];
-        Item expected = new Item ("Checker");
+        Item expected = new Item("Checker");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new Item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), "replaced item"
+        };
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleted() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("to be deleted");
+        tracker.add(item);
+        String[] answer = {
+                String.valueOf(item.getId())};
+        StartUI.delete(new StubInput(answer), tracker);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
 }
