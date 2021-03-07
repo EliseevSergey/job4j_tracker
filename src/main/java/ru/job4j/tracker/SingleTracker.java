3977ffa6657667b2622aeb29169a503201a1d10d
EliseevSergey <s.eliseev1@gmail.com>
2021-03-07 16:21:48 +0300

    2. Singleton. [#396612]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+    private static SingleTracker first = null;
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getFirst() {
+        if (first == null) {
+            first = new SingleTracker();
+        }
+        return first;
+    }
+
+    private Tracker tracker = new Tracker();
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item finfById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String name) {
+        return tracker.findByName(name);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
