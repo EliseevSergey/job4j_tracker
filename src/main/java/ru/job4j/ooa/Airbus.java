49316557daba3d4cc82d865379525f6f4a4898b2
sergeyeliseev <s.eliseev1@gmail.com>
2021-03-21 00:39:51 +0300

    4. final [#396616]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -1,37 +1,38 @@
 package ru.job4j.ooa;
 
 public class Airbus {
+    private static final int COUNT_ENGINE = 2;
     private String name;
-    private Aircraft aircraft = new Aircraft();
 
-    public Airbus(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void printModel() {
-        System.out.println("Модель самолета: " + name);
-    }
-
-    public void printCountEngine() {
-        if (this.getName().equals("A380")) {
-            System.out.println("Количество двигателей равно: " + (aircraft.getCOUNT_ENGINE() + 2));
-        } else {
-        System.out.println("Количество двигателей равно: " + aircraft.getCOUNT_ENGINE());
+        public Airbus(String name) {
+            this.name = name;
         }
-    }
 
-    @Override
-    public String toString() {
-        return "Airbus{" +
-                "name='" + name + '\'' +
-                '}';
-    }
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void printModel() {
+            System.out.println("Модель самолета: " + name);
+        }
+
+        public void printCountEngine() {
+            if (this.name.equals("A380")) {
+                System.out.println("Количество двигателей равно: " + (COUNT_ENGINE + 2));
+            }
+            else {
+                System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Airbus{"
+                    + "name='" + name + '\''
+                    + '}';
+        }
 }
--- src/main/java/ru/job4j/ooa/Aircraft.java
+++ src/main/java/ru/job4j/ooa/Aircraft.java
@@ -1,9 +1,7 @@
 package ru.job4j.ooa;
 
-public class Aircraft {
-    private final int COUNT_ENGINE = 2;
-
-    public int getCOUNT_ENGINE() {
-        return COUNT_ENGINE;
+public final class Aircraft {
+    public final void printModel() {
+        System.out.println("Метод выводит в консоль модель самолета.");
     }
 }
