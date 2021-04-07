e235d4a9b488532c3f7a8f3314402992f30463a9
EliseevSergey <s.eliseev1@gmail.com>
2021-04-07 14:20:04 +0300

    2. Очередь с приоритетом на LinkedList [#396556]

---
+++ src/main/java/ru/job4j/search/Task.java
@@ -0,0 +1,22 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class Task {
+    private String desc;
+    private int priority;
+
+    public Task(String desc, int priority) {
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
