ba653373b61fc0e45ae5bb0986d6bd2304312f97
EliseevSergey <s.eliseev1@gmail.com>
2021-06-15 22:49:48 +0300

    3. Компаратор для строк [#35008 #164843]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -6,41 +6,17 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
         int rsl = 0;
-        switch (Integer.compare(o1.length(), o2.length())) {
-            case (1):
-                for (int index = 0; index < o2.length(); index++) {
-                    int decision = Character.compare(o1.charAt(index), o2.charAt(index));
-                    if (decision != 0) {
-                        rsl = decision;
-                        break;
-                    }
-                    if (index == o2.length() - 1 && decision == 0) {
-                        rsl = 1;
-                    }
-                }
-                break;
-            case (-1):
-                for (int index = 0; index < o1.length(); index++) {
-                    int decision = Character.compare(o1.charAt(index), o2.charAt(index));
-                    if (decision != 0) {
-                        rsl = decision;
-                        break;
-                    }
-                    if (index == o1.length() - 1 && decision == 0) {
-                        rsl = -1;
-                    }
-                }
-                break;
-            case (0):
-                for (int index = 0; index < o1.length(); index++) {
-                    int decision = Character.compare(o1.charAt(index), o2.charAt(index));
-                    if (decision != 0) {
-                        rsl = decision;
-                        break;
-                    }
-                }
+        int shortest = Math.min(o1.length(), o2.length());
+        for (int index = 0; index < shortest; index++) {
+            int decision = Character.compare(o1.charAt(index), o2.charAt(index));
+            if (decision != 0) {
+                rsl = decision;
                 break;
             }
+            if (index == shortest - 1 && decision == 0) {
+                        rsl = Integer.compare(o1.length(), o2.length());
+                    }
+                }
     return rsl;
     }
 }
