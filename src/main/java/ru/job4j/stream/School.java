8441c598243e16c0531dee1ec36d5727a3e5fe0b
sergeyeliseev <s.eliseev1@gmail.com>
2022-03-20 07:51:24 +0300

    1. Фильтрация учеников [#110058 #277465]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -7,9 +7,8 @@ import java.util.stream.Collectors;
 
 public class School {
     public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> rsl = new ArrayList<>();
-        rsl = students.stream().filter(predict).collect(Collectors.toList());
-        rsl.forEach(System.out::println);
-        return rsl;
+        return students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
     }
 }
