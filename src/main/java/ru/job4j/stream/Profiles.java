2b14ae43c3ad1e54fd932f51d609e5696bbc74e6
sergeyeliseev <s.eliseev1@gmail.com>
2022-03-21 19:20:56 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,32 +1,24 @@
 package ru.job4j.stream;
 
-import net.sf.saxon.style.XSLOutput;
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.Comparator;
+import java.util.stream.Stream;
 
 public class Profiles {
+    public static Stream<Adress> adressPool(List<Profile> profiles) {
+        return profiles.stream().map(p -> p.getAdress());
+    }
+
     public static List<Adress> collect(List<Profile> profiles) {
-        return profiles.stream()
-                .map(p -> p.getAdress())
+        return Profiles.adressPool(profiles)
                 .collect(Collectors.toList());
     }
 
     public static List<Adress> collectSortWithoutDuplicate(List<Profile> profiles) {
-        Comparator<Adress> comparator = new Comparator<Adress>() {
-            @Override
-            public int compare(Adress o1, Adress o2) {
-                return o1.getCity().compareTo(o2.getCity());
-            }
-        };
-        return profiles.stream()
-               .map(p -> p.getAdress())
-               .sorted(comparator)
-               .distinct()
-               .collect(Collectors.toList());
+        Comparator<Adress> comparator = Comparator.comparing(a -> a.getCity());
+        return Profiles.adressPool(profiles).sorted(comparator)
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
