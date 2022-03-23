799b7611dac71289706a0701fcebce635048826c
sergeyeliseev <s.eliseev1@gmail.com>
2022-03-23 23:26:56 +0300

    2. Список адресов [#110062 #277486]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,13 +6,13 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Adress> collect(List<Profile> profiles) {
-        return profiles.stream().map(p -> p.getAdress())
+        return profiles.stream().map(Profile::getAdress)
                 .collect(Collectors.toList());
     }
 
     public static List<Adress> collectSortWithoutDuplicate(List<Profile> profiles) {
-        return profiles.stream().map(p -> p.getAdress())
-                .sorted(Comparator.comparing(a -> a.getCity()))
+        return profiles.stream().map(Profile::getAdress)
+                .sorted(Comparator.comparing(Adress::getCity))
                 .distinct()
                 .collect(Collectors.toList());
     }
