fe589e49c7e40779f25e7d8095b97b85a8663af0
sergeyeliseev <s.eliseev1@gmail.com>
2022-04-05 21:21:04 +0300

    0. Stream API улучшения [#58167 #284700]

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -3,7 +3,6 @@ package ru.job4j.stream;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StudentLevel {
     public static List<Student> levelOf(List<Student> students, int bound) {
