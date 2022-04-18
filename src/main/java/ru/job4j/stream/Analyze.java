98b6350de0ea1fc9c036c0a0167919fe2c12f2ea
EliseevSergey <s.eliseev1@gmail.com>
2022-04-18 14:33:18 +0300

    transfert2

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -3,15 +3,18 @@ package ru.job4j.stream;
 import net.sf.saxon.functions.Empty;
 import ru.job4j.stream.Pupil;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.Objects;
 
 public class Analyze {
     public static double averageScore(Stream<Pupil> stream) {
-/*        return stream.flatMap(item -> item.getSubjects()
-                .stream()
-                .mapToInt(sub -> sub.getScore()).average());*/
-        return stream.flatMap(pupil -> )
-
-
+        return stream.flatMap(pupil -> pupil.getSubjects()
+                .stream())
+                .mapToInt(item -> item.getScore())
+                .average()
+                .orElse(0D);
     }
 }
