616cde8f32995b1c657889ff8a3aa61788e8f9bb
Sergey Eliseev <s.eliseev1@gmail.com>
2023-05-07 21:12:20 +0300

    1. Профилирование. [#1591]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -41,6 +41,8 @@ public class StartUI {
                     new ShowAllAction(output),
                     new FindByIdAction(output),
                     new FindByNameAction(output),
+                    new CreateManyItems(output),
+                    new DeleteAllItems(output),
                     new ExitAction(output)
             );
             new StartUI(output).init(input, tracker, actions);
