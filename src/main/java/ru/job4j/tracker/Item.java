78152d6e024e13914e5de320d640be58aea6b5fd
Sergey Eliseev <s.eliseev1@gmail.com>
2025-12-08 22:27:40 +0300

    1. Lombok. Настройка IDEA [#504888 #555907]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,10 @@
 package ru.job4j.tracker;
 
+import lombok.Data;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
+@Data
 public class Item {
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
@@ -12,57 +13,11 @@ public class Item {
     private LocalDateTime created = LocalDateTime.now().withNano(0);
 
     public Item(String name) {
-    this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id; }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public void setCreated(LocalDateTime created) {
-        this.created = created;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id && Objects.equals(name, item.name) && created.equals(item.created);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, created);
     }
 
     @Override
     public String toString() {
         return String.format("id: %s, name: %s, created: %s", id, name, FORMATTER.format(created));
     }
-
-    /*@Override
-    public String toStringTest() {
-        return String.format("id: %s, name: %s, created: %s", id, name, FORMATTER.format(created));
-    }*/
-
 }
