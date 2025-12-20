08416eef1232e43d91cc95620d9ba99a022f9d23
Sergey Eliseev <s.eliseev1@gmail.com>
2025-12-20 08:24:40 +0300

    transfert

---
+++ db/scripts/002_dml_insert_items_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO items (name, created) VALUES ('Item1', '2022-08-24T15:59:57');
+INSERT INTO items (name, created) VALUES ('Item2', '2022-08-24T15:59:57');
+INSERT INTO items (name, created) VALUES ('Item3', '2022-08-24T15:59:57');
---
+++ src/main/java/ru/job4j/tracker/HQLUsage.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.*;
+
+import javax.persistence.Query;
+
+public class HQLUsage {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try (SessionFactory sf = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory()) {
+            Session session = sf.openSession();
+            /*bla bla with session*/
+
+            Query query = session.createQuery(
+                    "FROM Item AS i WHERE i.id = 8", Item.class);
+            System.out.println(((org.hibernate.query.Query<?>) query).uniqueResult());
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+
+
+    }
+}
+
